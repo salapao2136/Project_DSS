c9d1aa36172a80ff5921a4a69dd5635aaf734972
kingkong64 <kingkong2103@gmail.com>
2016-05-10 21:08:45 +0700

    enable cors

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.set('port', (process.env.PORT || 3000))
 
 app.use(express.static('public'))
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 var exec = require('child_process').exec
 
 app.post('/data', jsonParser, function (req, res) {
